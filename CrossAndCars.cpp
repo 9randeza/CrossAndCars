1a3acaab7b412020ff9fb966cd90f3cc9bf16643
9randeza <geydarb0412@gmail.com>
2024-03-31 01:11:00 +0400

    very bad programm

--- CrossAndCars.cpp
+++ CrossAndCars.cpp
@@ -11,17 +11,15 @@ struct ManyCarsInOneRoad{
 
 struct ManyCarsInCross{
 	std::string message;
-     ManyCarsInCross(const std::string& msg) : message(msg) {}
+    ManyCarsInCross(const std::string& msg) : message(msg) {}
 };
 
 void out(int N, int S, int W, int E){
-	std::mutex m;
-	std::lock_guard<std::mutex> lg(m);
 	if(N != 0){
-	std::cout << "car drive on road 1\n";
+		std::cout << "car drive on road 2\n";
 	}
 	if(S != 0){
-		std::cout << "car drive on road 2\n";
+		std::cout << "car drive on road 1\n";
 	}
 	if(E != 0){
 		std::cout << "car drive on road 3\n";
@@ -39,20 +37,21 @@ void road(int N, int S, int E, int W){
 		std::cout << "green on road B\n";
 	}
 	
-	while(N > 0){
-		std::jthread pusht(out, N, S, W, E);
-		N--;
-	}
+
 	while(S > 0){
-		std::jthread pusht1(out, N, S, W, E);
+		std::jthread pusht1(out, 0, S, 0, 0);
 		S--;
 	}
+	while(N > 0){
+		std::jthread pusht(out, N, 0, 0, 0);
+		N--;
+	}
 	while(W > 0){
-		std::jthread pusht2(out, N, S, W, E);
+		std::jthread pusht2(out, 0, 0, W, 0);
 		W--;
 	}
 	while(E > 0){
-		std::jthread pusht3(out, N, S, W, E);
+		std::jthread pusht3(out, 0, 0, 0, E);
 		E--;
 	}
 	
@@ -90,7 +89,7 @@ void ss_check(int i, std::deque<int>::iterator start, std::deque<int>::iterator 
 				}
 				it--;
 			}else{
-				throw ManyCarsInOneRoad("Many cars on the road s!");
+				throw ManyCarsInOneRoad("Many cars on the road S!");
 			}
 		}
 		if(*it == 3){
@@ -124,12 +123,12 @@ void ss_check(int i, std::deque<int>::iterator start, std::deque<int>::iterator 
     
 	}
 	
-road(N, S, E, W);
+	road(N, S, E, W);
 }
 
 int main(){
-	std::deque<int> t = {1, 1, 1, 1, 2, 2};
-	std::deque<int> ss = {1, 1, 1, 2, 3, 4};
+	std::deque<int> t = {1, 1, 1, 1};
+	std::deque<int> ss = {1, 2, 1, 2};
 	int o = 1;
 	while(!t.empty()){
 		if(t[0] == t[1]){
@@ -153,3 +152,4 @@ int main(){
 		}
 	} 
 }
+
